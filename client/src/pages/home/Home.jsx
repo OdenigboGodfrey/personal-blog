9d8987ef2839b8ea258f66cd8145bff450cc99ec
decentralizedmvp <decentralizedmvp@gmail.com>
2021-08-28 16:08:40 -0400

    refactor(home): move state up to HomeWrapper

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, Container, Row, Col, Button } from 'react-bootstrap';
 import CreatePostModal from './create-post-modal/CreatePostModal';
 import getImageForPost from '../utils/get-image-for-post';
@@ -6,34 +6,18 @@ import getImageForPost from '../utils/get-image-for-post';
 import './Home.css';
 
 export function Home({
+  closeModal,
+  onChange,
+  onSubmit,
+  modalVisible,
+  openModal,
   postTitles
 }) {
-  const [createPostForm, setCreatePostForm] = useState({ post: '', postTitle: '' });
-  const [modalVisible, setModalVisible] = useState(false);
-
-  /**
-   * @description On change handler for post modal
-   * @param {Object} event
-   */
-  function onChange(event) {
-    const { target } = event;
-    setCreatePostForm(previousState => ({ ...previousState, [target.name]: target.value }));
-  }
-
-  /**
-   * @description Submit handler for new post
-   * @param {Object} event 
-   */
-  function onSubmit(event) {
-    event.preventDefault();
-    console.log(createPostForm);
-  }
-
   return (
     <Container fluid="lg" className="home">
       <CreatePostModal
         onChange={onChange}
-        onClose={() => setModalVisible(false)}
+        onClose={closeModal}
         onSubmit={onSubmit}
         show={modalVisible}
       />
@@ -43,7 +27,7 @@ export function Home({
           <div className="home-button-header-container">
             <h2>Welcome to the Blockchain Blog</h2>
             <p>A decentralized blog running on the blockchain.</p>
-            <Button onClick={() => setModalVisible(true)} variant="primary">Create Post</Button>
+            <Button onClick={openModal} variant="primary">Create Post</Button>
           </div>
         </Col>
       </Row>
@@ -84,7 +68,48 @@ function PostMetadata({
 }
 
 export function HomeWrapper() {
+  const [createPostForm, setCreatePostForm] = useState({ post: '', postTitle: '' });
+  const [loading, setLoading] = useState(true);
+  const [modalVisible, setModalVisible] = useState(false);
+  // posts' titles
+  const [posts, setPosts] = useState([]);
+
+  /**
+   * @description On change handler for post modal
+   * @param {Object} event
+   */
+  function onChange(event) {
+    const { target } = event;
+    setCreatePostForm(previousState => ({ ...previousState, [target.name]: target.value }));
+  }
+
+  /**
+   * @description Submit handler for new post
+   * @param {Object} event 
+   */
+  function onSubmit(event) {
+    event.preventDefault();
+    console.log(createPostForm);
+  }
+
+  useEffect(() => {
+    (async function() {
+      // mock
+      setPosts(['First Post', 'Second Post', 'Third Post', 'Fourth Post']);
+      setLoading(true);
+    })();
+  }, []);
+
   return (
-    <Home/>
+    loading ?
+      <Home
+        closeModal={() => setModalVisible(false)}
+        onChange={onChange}
+        onSubmit={onSubmit}
+        modalVisible={modalVisible}
+        openModal={() => setModalVisible(true)}
+        postTitles={posts}
+      /> :
+      <div>loading...</div>
   );
 }
