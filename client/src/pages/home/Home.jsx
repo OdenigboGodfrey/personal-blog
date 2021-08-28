cc6f8786c6356c9273866bf20415865f4d50d89c
decentralizedmvp <decentralizedmvp@gmail.com>
2021-08-28 15:56:32 -0400

    refactor: update utility fn to be imported rather than declared in file

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Card, Container, Row, Col, Button } from 'react-bootstrap';
 import CreatePostModal from './create-post-modal/CreatePostModal';
+import getImageForPost from '../utils/get-image-for-post';
 
 import './Home.css';
 
@@ -80,28 +81,6 @@ function PostMetadata({
       </Card.Body>
     </Card>
   );
-}
-
-/**
- * @description Helper function to get the image for a post
- * @param {Number} postIndex
- * @returns {String}
- */
-function getImageForPost(postIndex) {
-  const postImages = [
-    "https://decentralized-mvp.s3.amazonaws.com/blog/post-photo-1.jpg",
-    "https://decentralized-mvp.s3.amazonaws.com/blog/post-photo-2.jpg",
-    "https://decentralized-mvp.s3.amazonaws.com/blog/post-photo-3.jpg"
-  ];
-  const normalizedPostIndex = postIndex + 1;
-
-  if (normalizedPostIndex % 3 === 0) {
-    return postImages[2];
-  } else if (normalizedPostIndex % 2 === 0) {
-    return postImages[1];
-  } else {
-    return postImages[0];
-  }
 }
 
 export function HomeWrapper() {
