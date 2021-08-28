2f583274ed713dbfcbf47af80e882d59a3b87853
decentralizedmvp <decentralizedmvp@gmail.com>
2021-08-28 16:20:17 -0400

    feat: add support for post route

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,16 @@ import {
   Route
 } from 'react-router-dom';
 import { HomeWrapper as Home } from './pages/home/Home';
+import { PostWrapper as Post } from './pages/post/Post';
 import NoMatch from './pages/no-match/NoMatch';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/post/:postIndex">
+          <Post/>
+        </Route>
         <Route path="/">
           <Home/>
         </Route>
